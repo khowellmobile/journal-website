145dec743264eeaa67e7e77b9f5615b29f140b28
khowellmobile <khowellmobile@gmail.com>
2024-12-11 13:14:20 -0500

    Added basic card for a post

--- frontend/journal-app/src/components/elements/PostCard.jsx
+++ frontend/journal-app/src/components/elements/PostCard.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 import classes from "./PostCard.module.css";
 
 const PostCard = () => {
-    return <div className={classes.card}></div>;
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => {
+        setIsEditing(!isEditing);
+    };
+
+    return (
+        <div className={classes.card}>
+            <div className={classes.header}>
+                <div className={classes.headerText}>
+                    <h3>Working on website</h3>
+                    <p>Date: 12/11/2024 @ 10:59am</p>
+                </div>
+                <div className={classes.headerTools}>
+                    <div className={classes.tool} onClick={handleEditClick}>{isEditing ? <p>✔</p> : <p>E</p>}</div>
+                    <div className={classes.tool}><p>x</p></div>
+                </div>
+            </div>
+            <div className={classes.content}>
+                <textarea type="text" className={classes.textarea} placeholder="Type here"></textarea>
+            </div>
+        </div>
+    );
 };
 
 export default PostCard;
---
+++ frontend/journal-app/src/components/elements/TextArea.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import classes from "./TextArea.module.css";
+
+const TextArea = () => {
+    return (
+        <div>
+            <textarea
+                type="text"
+                className={classes.textarea}
+                spellcheck="false"
+                placeholder="Type here"
+            ></textarea>
+        </div>
+    );
+};
+
+export default TextArea;
