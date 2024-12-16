322d45d6f54d98e1294d78bdcaee571ed51b4a65
khowellmobile <khowellmobile@gmail.com>
2024-12-16 13:00:04 -0500

    Emsure proper env variable naming

--- frontend/journal-app/src/components/clients/supabaseClient.jsx
+++ frontend/journal-app/src/components/clients/supabaseClient.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const VITE_SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Create a Supabase client
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
